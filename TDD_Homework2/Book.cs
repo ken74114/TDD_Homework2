8a6f5220d83e7bb01587da67f04d7dc35bd4a373
kenhuang <kenhuang@KEN-PC>
2016-07-14 10:32:01 +0800

    調整(重構)

--- TDD_Homework2/Book.cs
+++ TDD_Homework2/Book.cs
@@ -1,8 +1,25 @@
-﻿namespace TDD_Homework2
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TDD_Homework2
 {
+    [Serializable]
     public class Book
     {
         public string BookName { get; set; }
         public int Price { get; set; }
+
+        public Book Clone()
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memory, this);
+                memory.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(memory) as Book;
+            }
+        }
     }
 }
--- TDD_Homework2/CalculatorBook.cs
+++ TDD_Homework2/CalculatorBook.cs
@@ -7,18 +7,36 @@ namespace TDD_Homework2
 {
     public class CalculatorBook
     {
-        private double[] distinct = new double[] { 1, 0.95, 0.9, 0.8, 0.75 };
+        private Dictionary<int, double> Discount
+        {
+            get
+            {
+                Dictionary<int, double> discount = new Dictionary<int, double>();
+                discount.Add(1, 1);
+                discount.Add(2, 0.95);
+                discount.Add(3, 0.9);
+                discount.Add(4, 0.8);
+                discount.Add(5, 0.75);
+                return discount;
+            }
+        }
 
         public int CalculatorPrice(List<Book> books)
         {
             int total = 0;
-            var differenceBooks = books.Distinct(new BookCompare()).ToList();
-            differenceBooks.Select(x => books.Remove(x)).ToList();
-            if (books.Count > 0)
+            var allBooks = books.Select(x => x.Clone()).ToList();
+            var differenceBooks = allBooks.Distinct(new BookCompare()).ToList();
+            differenceBooks.ForEach(x => allBooks.Remove(x));
+            if (allBooks.Count > 0)
             {
-                total += this.CalculatorPrice(books);
+                total += this.CalculatorPrice(allBooks);
             }
-            return total += Convert.ToInt32(differenceBooks.Sum(x => x.Price) * distinct[differenceBooks.Count - 1]);
+            return total += CalculateAmount(differenceBooks);
+        }
+
+        private int CalculateAmount(List<Book> books)
+        {
+            return Convert.ToInt32(books.Sum(x => x.Price) * Discount[books.Count()]);
         }
     }
 
